404e45363d31978d7fad9302eede26f715f10325
peter.cho <peter.cho@kakaocorp.com>
2020-07-21 07:26:44 +0900

    설명 수정

--- docs/src/vue2-component/guide/mount.js
+++ docs/src/vue2-component/guide/mount.js
@@ -1,9 +1,11 @@
 import {ButtonComponent} from './ButtonComponent.js';
 import {CounterComponent} from './CounterComponent.js';
 import {ReactiveComponent} from './ReactiveComponent.js';
+import {InputComponent} from './InputComponent.js';
 
 document
   .querySelector('#app')
   .appendChild(ButtonComponent())
   .appendChild(CounterComponent())
-  .appendChild(ReactiveComponent());
+  .appendChild(ReactiveComponent())
+  .appendChild(InputComponent());
