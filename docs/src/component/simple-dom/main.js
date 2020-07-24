1d1c822c5906f3be9f1a5517d67776934985dd28
peter.cho <peter.cho@kakaocorp.com>
2020-07-25 02:57:11 +0900

    설명 수정

--- docs/src/component/simple-dom/main.js
+++ docs/src/component/simple-dom/main.js
@@ -2,14 +2,39 @@ import {parse} from './core/parse.js';
 import {generate} from './core/generate.js';
 import {patch} from './core/fragment-dom-20200725/patch.js';
 
-const template = (state) => `${state.text}
-  <input type="text" @input="onInput">
-  <input type="text" value="${state.text}">`;
+const template = (state) => {
+  return `<div>
+    <h1>Todo</h1>
+    <input 
+      type="text"
+      @keyup="onKeyUp"
+      @input="onInput"
+      value="${state.text}"
+    >
+    <button type="button" @click="onClick">추가</button>
+    <p>작성중: ${state.text}</p>
+    <h2>Todo List</h2>
+    <ol>
+      ${state.todo.map(todo => `<li>${todo}</li>`).join('')}
+    </ol>
+  </div>`
+};
 
 const fns = {
-  onInput: (event) => {
-    state.setText(event.target.value);
-  }
+  addTodo: () => {
+    if (!state.text) {
+      return;
+    }
+    state.setTodo(state.text);
+    state.setText('');
+  },
+  onKeyUp: (event) => {
+    if (event.key.toLowerCase() === 'enter') {
+      fns.addTodo(state.text);
+    }
+  },
+  onClick: event => fns.addTodo(),
+  onInput: event => state.setText(event.target.value)
 };
 
 const actualDOM = document.querySelector('#app');
@@ -21,10 +46,15 @@ const update = (state) => {
 
 const state = {
   text: '',
+  todo: [],
   setText: (text) => {
     state.text = text;
     update(state);
-  }
+  },
+  setTodo: (todo) => {
+    state.todo = [...state.todo, todo];
+    update(state);
+  },
 };
 
 update(state);
